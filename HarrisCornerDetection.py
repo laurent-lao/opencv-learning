71cda76b22af3ac06252b91b2f0ea7661bbd068b
laurentlaurent <lao.laurent@gmail.com>
2018-10-22 14:44:01 -0400

    Added latest files

---
+++ HarrisCornerDetection.py
@@ -0,0 +1,164 @@
+# Author: Laurent Lao
+# For: UAV Concordia Sprint Demo on Oct 19 2018
+# Date created: Oct 18, 2018
+# Purpose: Test Harris Corner Detection as per tutorial on
+# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html#harris-corners
+# The program:
+# Needs a file argument and will locate some corners on the provided images
+
+import cv2
+import numpy as np
+import argparse
+import sys
+
+
+def main(input_file):
+
+    is_continue = 0
+    while is_continue != 9:
+
+        # displays menu
+        is_continue = display_menu()
+
+        if is_continue != 9:
+
+            # input file
+            file_to_process = input_file
+
+            # converts file into greyscale then into a float32
+            img = cv2.imread(file_to_process)
+            greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            greyscale = np.float32(greyscale)
+
+            # dst creates an array containing the transformed greyscale'd and corners
+            dst = cv2.cornerHarris(greyscale, corner_harris_block_size, corner_harris_k_size, corner_harris_k)
+
+            # result of dst is dilated for marking the corners, not important
+            # as per OpenCV2 tutorial
+            dst = cv2.dilate(dst, None)
+
+            # Threshold the greyscale'd img according to the array given by dst and an optimal value
+            # Mark higher than threshold stuff blue?
+            img[dst > dst_percentage*dst.max()] = dst_colour
+
+            # Show the result img
+            print("Press Escape to close windows")
+            cv2.imshow('dst', img)
+            if cv2.waitKey(0) & 0xff == 27:
+                cv2.destroyAllWindows()
+
+        else:
+            print("End of Program")
+            exit(0)
+
+
+def display_menu(): # displays a menu for changing variables
+
+    # initializing variables
+    global corner_harris_block_size, corner_harris_k_size, corner_harris_k, dst_percentage, dst_colour, colour_label
+    str_block_size = "Block Size value"
+    str_k_size = "Aperture Parameter value"
+    str_k = "Free Parameter value"
+    str_dst = "Sensitivity of Corner"
+    str_colour = "Colour of corner detection"
+
+    while True:
+        # displaying menu options
+        print("\nMenu options for Harris Corner Detection")
+        print("0:\tConfirm values and Render")
+        print("1:\tReset to default values")
+        print("2:\t" + str_block_size + ": \t\t\t" + str(corner_harris_block_size) + "\t\tdefault is: 2")
+        print("3:\t" + str_k_size + ": \t" + str(corner_harris_k_size) + "\t\tdefault is: 3")
+        print("4:\t" + str_k + ": \t\t" + str(corner_harris_k) + "\tdefault is: 0.04")
+        print("5:\t" + str_dst + ": \t\t" + str(dst_percentage) + "\tdefault is: 0.01")
+        print("6:\t" + str_colour + ": " + colour_label + "\t\tdefault is: red")
+        print("9:\tEnd the Program\n")
+
+        # prompting and changing as per menu behaviour
+        menu_option = -1
+        while menu_option < 0 or (menu_option > 6 and menu_option != 9):
+            menu_option = int(input("Enter a menu option or the value to be changed: "))
+        if menu_option == 0:
+            return 0
+        elif menu_option == 1:
+            set_default_value()
+        elif menu_option == 9:
+            return 9
+        elif menu_option == 6:
+            switch_colours()
+        else:
+            new_value = input("Enter new value: ")
+            if menu_option == 2:
+                corner_harris_block_size = int(new_value)
+            elif menu_option == 3:
+                corner_harris_k_size = int(new_value)
+            elif menu_option == 4:
+                corner_harris_k = float(new_value)
+            elif menu_option == 5:
+                dst_percentage = float(new_value)
+            else:
+                exit(1)
+
+
+def set_default_value():
+
+    # resetting into default values
+    global corner_harris_block_size, corner_harris_k_size, corner_harris_k, dst_percentage, dst_colour, colour_label
+    corner_harris_block_size = default_value[0]  # blockSize : size of neighbourhood for C.Detect (default = 2)
+    corner_harris_k_size = default_value[1]  # k size    : Aperture parameter for Sobel derivative (default = 3)
+    corner_harris_k = default_value[2]  # k         : Harris detector free parameter (default = 0.04)
+    dst_percentage = default_value[3]  # threshold : Percentage for the sensitivity of corner (default = 0.01)
+    dst_colour = colour_red
+    colour_label = "red"
+
+
+def switch_colours():
+    #declare variable
+    global  dst_colour, colour_red, colour_blue, colour_green, colour_label
+
+    # show menu
+    print("Switch the colour to:")
+    print("1:\tRed")
+    print("2:\tBlue")
+    print("3:\tGreen")
+
+    # prompt user and behaviour
+    menu_input = 0
+    while menu_input < 1 or menu_input > 3:
+        menu_input = int(input("Enter your choice: "))
+    if menu_input == 1:
+        dst_colour = colour_red
+        colour_label = "red"
+    elif menu_input == 2:
+        dst_colour = colour_blue
+        colour_label = "blue"
+    elif menu_input == 3:
+        dst_colour = colour_green
+        colour_label = "green"
+    else:
+        exit(2)
+
+
+# parsing the filename into the main function
+parser = argparse.ArgumentParser(description="Corner detection of image using Harris Corner Detection")
+parser.add_argument("filename", help="Name of the image in jpg")
+args = parser.parse_args()
+
+# define variables
+default_value = [2, 3, 0.04, 0.01]
+colour_red = [0, 0, 255]
+colour_green = [0, 255, 0]
+colour_blue = [255, 0, 0]
+
+corner_harris_block_size = default_value[0]  # blockSize : size of neighbourhood for C.Detect (default = 2)
+corner_harris_k_size = default_value[1]      # k size    : Aperture parameter for Sobel derivative (default = 3)
+corner_harris_k = default_value[2]           # k         : Harris detector free parameter (default = 0.04)
+dst_percentage = default_value[3]            # threshold : Percentage for the sensitivity of corner (default = 0.01)
+dst_colour = colour_red                      # colour    : Colour for corner marking (default = [0,0,255] (RED)
+colour_label = "red"
+
+# running main
+main(args.filename)
+
+# TODO
+# * Fix error messages
---
+++ ImageThresholding.py
@@ -0,0 +1,181 @@
+# Author: Laurent Lao
+# For: UAV Concordia Sprint Demo on Oct 19 2018
+# Date created: Oct 18, 2018
+# Purpose: Test different image thresholding as per tutorial on
+# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_thresholding/py_thresholding.html#thresholding
+# The program:
+# Needs a file and a choice (1 or 2) argument, then either shows simple image thresholding or adaptive
+
+import cv2
+import numpy as np
+import argparse
+from matplotlib import pyplot as plt
+
+
+def main(input_file, simple_or_adaptive):
+
+    # checking arguments to see which threshold to perform and pass the filename
+    is_continue = 0
+    while is_continue != 9:
+        is_continue = display_menu(simple_or_adaptive)
+        if is_continue != 9:
+            if simple_or_adaptive == 1:
+                simple(input_file)
+            elif simple_or_adaptive == 2:
+                adaptive(input_file)
+            else:
+                print("Unexpected Error")
+    print("End of program")
+    exit(0)
+
+
+def simple(input_file):  # simple uses a global value for Binary, Binary_inv, Trunc, To Zero and To Zero_inv
+
+    # reading the image and threshold via Global values
+    # threshold in several ways as to show the differences
+    img = cv2.imread(input_file, 0)
+    ret,thresh1 = cv2.threshold(img, threshold_number, threshold_max, cv2.THRESH_BINARY)
+    ret,thresh2 = cv2.threshold(img, threshold_number, threshold_max, cv2.THRESH_BINARY_INV)
+    ret,thresh3 = cv2.threshold(img, threshold_number, threshold_max, cv2.THRESH_TRUNC)
+    ret,thresh4 = cv2.threshold(img, threshold_number, threshold_max, cv2.THRESH_TOZERO)
+    ret,thresh5 = cv2.threshold(img, threshold_number, threshold_max, cv2.THRESH_TOZERO_INV)
+
+    # creating array for matplotlib
+    titles = ["Original Image", "BINARY", "BINARY_INV", "TRUNC", "TOZERO", "TOZERO_INV"]
+    images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+    # creating the matplotlib plot of images
+    for i in range(6):
+        plt.subplot(2, 3, i + 1), plt.imshow(images[i], "gray")
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+
+    plt.show()
+    return
+
+
+def adaptive(input_file):    # in adaptive threshold, image is partitioned and a mean value is used for threshold
+
+    # reading the image and adding a blur
+    img = cv2.imread(input_file, 0)
+    img = cv2.medianBlur(img, 5)
+
+    # doing both thresholds Mean and Gaussian
+    th1 =  cv2.adaptiveThreshold(img, adaptive_thresh_max, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
+                                 adaptive_block_size, adaptive_constant)
+    th2 = cv2.adaptiveThreshold(img, adaptive_thresh_max, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+                                adaptive_block_size, adaptive_constant)
+
+    # creating array for matplotlib
+    titles = ["Original Image", "Adaptive Mean Thresholding", "Adaptive Gaussian Thresholding"]
+    images = [img, th1, th2]
+
+    # creating the matplotlib plot of images
+    for i in range(3):
+        plt.subplot(3, 1, i + 1), plt.imshow(images[i], "gray")
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+
+    plt.show()
+    return
+
+
+def display_menu(simple_or_adaptive):     # menu for changing variables and such
+
+    # defining string variables
+    str_simple_global = "Global Threshold Value"
+    str_simple_max = "Max Threshold Value"
+    str_adaptive_block_size = "Block Size of Partitions"
+    str_adaptive_constant = "Constant substracted from Mean"
+    str_adaptive_max = "Max Threshold Value"
+
+    # passing globals
+    global threshold_number, threshold_max, adaptive_block_size, adaptive_constant, adaptive_thresh_max
+
+    # check if simple or adaptive
+    if simple_or_adaptive == 1:
+        str_type = "Simple Thresholding"
+    else:
+        str_type = "Adaptive Thresholding"
+
+    # menu options
+    is_done = -1
+    while is_done != 0:
+        print("\nMenu options for " + str_type + "\n")
+        print("0:\tConfirm value and render")
+        print("1:\tReset to default values")
+        # menu for simple thresholding
+        if simple_or_adaptive == 1:
+            print("2:\tThe value for " + "\"" + str_simple_global + "\"" + ": \t" + str(threshold_number) +
+                  "\t default: " + str(default_simple[0]))
+            print("3:\tThe value for " + "\"" + str_simple_max + "\"" + ": \t\t" + str(threshold_max) +
+                  "\t default: " + str(default_simple[1]))
+        if simple_or_adaptive == 2:
+            print("2:\tThe value for " + "\"" + str_adaptive_block_size + "\"" + ": \t" + str(adaptive_block_size) +
+                  "\t default: " + str(default_adaptive[1]))
+            print("3:\tThe value for " + "\"" + str_adaptive_constant + "\"" + ": \t" + str(adaptive_constant) +
+                  "\t default: " + str(default_adaptive[2]))
+            print("4:\tThe value for " + "\"" + str_adaptive_max + "\"" + ": \t" + str(adaptive_thresh_max) +
+                  "\t default: " + str(default_adaptive[0]))
+        print("9:\tEnd the Program")
+
+        # prompting user for menu option
+        print("\n")
+        menu_choice = -1
+        while((simple_or_adaptive == 1 and (menu_choice < 0 or (menu_choice > 3 and menu_choice != 9))) or
+              (simple_or_adaptive == 2 and (menu_choice < 0 or (menu_choice > 4 and menu_choice != 9)))):
+            str_menu_choice = input("Enter the menu option or the value you wish to change: ")
+            menu_choice = int(str_menu_choice)
+
+        # behaviour according to menu_choice
+        if menu_choice == 0:
+            return 0
+        elif menu_choice == 9:
+            return 9
+        if simple_or_adaptive == 1:
+            if menu_choice == 1:
+                threshold_number = default_simple[0]
+                threshold_max = default_simple[1]
+            else:
+                new_value = int(input("New value: "))
+                if menu_choice == 2:
+                    threshold_number = new_value
+                elif menu_choice == 3:
+                    threshold_max = new_value
+        if simple_or_adaptive == 2:
+            if menu_choice == 1:
+                adaptive_thresh_max = default_adaptive[0]
+                adaptive_block_size = default_adaptive[1]
+                adaptive_constant = default_adaptive[2]
+            else:
+                new_value = int(input("New value: "))
+                if menu_choice == 2:
+                    adaptive_block_size = new_value
+                elif menu_choice == 3:
+                    adaptive_constant = new_value
+                elif menu_choice == 4:
+                    adaptive_thresh_max = new_value
+
+
+# parsing the filename into the main function
+parser = argparse.ArgumentParser(description="Image Threshold")
+parser.add_argument("filename", help="Name of the image in jpg")
+parser.add_argument("simpleOrAdaptive", type=int, choices=[1, 2], help="Enter 1 for Simple, 2 for Adaptive")
+args = parser.parse_args()
+
+# define global variables
+default_simple = [127, 255]
+default_adaptive = [255, 11, 2]
+# defining simple thresholding
+threshold_number = default_simple[0]            # number    : threshold of pixel classification
+threshold_max = default_simple[1]               # max       : pixel is assigned max if over threshold
+# defining adaptive thresholding variables
+adaptive_thresh_max = default_adaptive[0]       # max           : pixel value is assigned max if over mean
+adaptive_block_size = default_adaptive[1]       # block size    : decides the size of the partition
+adaptive_constant = default_adaptive[2]         # C             : constant which is subtracted from the mean values
+
+# adding into main
+main(args.filename, args.simpleOrAdaptive)
+
+# TODO
+# * fix error message
